a2ac789d3f62b73cdf1db7ead9bf50f5aeb72725
Bijay Kumar Yadav <bijay.yadav@cloudyfox.com>
2020-01-16 15:42:55 +0545

    Var Vs Let

---
+++ tutorial5.ts
@@ -0,0 +1,12 @@
+function greetPerson(name: string) {
+    if(name === "Chandler") {
+        var greet = "Hello Chandler"
+    }
+    else {
+        var greet = "Hi there";
+    }
+
+    console.log(greet);
+}
+
+greetPerson("Chandler");
